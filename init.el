7c72a724ce9e5941b57f32f7a1c7af4923ea4c31
Tu Do <tu.h.do@dektech.com.au>
2014-08-26 14:36:23 +0700

    Revert previous commit

--- init.el
+++ init.el
@@ -40,15 +40,12 @@
 ;; function-args
 (require 'function-args)
 (fa-config-default)
-(define-key c-mode-map  [(control tab)] 'moo-complete)
-(define-key c++-mode-map  [(control tab)] 'moo-complete)
+(define-key c-mode-map  [(tab)] 'moo-complete)
+(define-key c++-mode-map  [(tab)] 'moo-complete)
 
 ;; company
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-(define-key c-mode-map [(tab)] 'company-complete)
-(define-key c++-mode-map [(tab)] 'company-complete)
-
 
 ;; company-c-headers
 (add-to-list 'company-backends 'company-c-headers)
