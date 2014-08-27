742194672064331e7c126b9d1e729a65285fd114
Tu, Do <tuhdo1710@gmail.com>
2014-08-28 01:10:54 +0700

    Swap moo-complete and company-clang key bindings

--- init.el
+++ init.el
@@ -45,12 +45,14 @@
 ;; function-args
 (require 'function-args)
 (fa-config-default)
-(define-key c-mode-map  [(tab)] 'moo-complete)
-(define-key c++-mode-map  [(tab)] 'moo-complete)
+(define-key c-mode-map  [(control tab)] 'moo-complete)
+(define-key c++-mode-map  [(control tab)] 'moo-complete)
 
 ;; company
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+(define-key c-mode-map  [(tab)] 'company-clang)
+(define-key c++-mode-map  [(control tab)] 'company-clang)
 
 ;; company-c-headers
 (add-to-list 'company-backends 'company-c-headers)
