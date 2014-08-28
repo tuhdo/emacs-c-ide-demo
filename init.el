39b91a613479eaa3ef5251b88eefcce194f39530
Tu Do <tu.h.do@dektech.com.au>
2014-08-28 10:56:45 +0700

    Revert swapping

--- init.el
+++ init.el
@@ -45,15 +45,15 @@
 ;; function-args
 (require 'function-args)
 (fa-config-default)
-(define-key c-mode-map  [(control tab)] 'moo-complete)
-(define-key c++-mode-map  [(control tab)] 'moo-complete)
+(define-key c-mode-map  [(tab)] 'moo-complete)
+(define-key c++-mode-map  [(tab)] 'moo-complete)
 
 ;; company
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (delete 'company-semantic company-backends)
-(define-key c-mode-map  [(tab)] 'company-complete)
-(define-key c++-mode-map  [(tab)] 'company-complete)
+(define-key c-mode-map  [(control tab)] 'company-complete)
+(define-key c++-mode-map  [(control tab)] 'company-complete)
 
 ;; company-c-headers
 (add-to-list 'company-backends 'company-c-headers)
