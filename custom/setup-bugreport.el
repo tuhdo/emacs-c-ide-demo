4996526ae977c9b19e6028764f6a7d8e1b1fb95b
shizhaohui <shizhaohui@xiaomi.com>
2018-08-30 13:40:39 +0800

    add android-trace-mode filename pattern && add some keywork

--- custom/setup-bugreport.el
+++ custom/setup-bugreport.el
@@ -1,5 +1,5 @@
 
-
+;; reference http://ergoemacs.org/emacs/elisp_syntax_coloring.html
 
 (defun android-trace-modify ()
   "Better to show android traces file with org mode."
@@ -23,14 +23,15 @@
 
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
+(defvar android-trace-font-lock-keywords nil "Syntax table for `android-trace-mode'.")
 (setq android-trace-font-lock-keywords
       (let* (
              ;; define several category of keywords
-             (x-keywords '("Cmd line" "daemon" "sysTid" "utm" "stm" "tid" "at" "native" "kernel"))
-             (x-types '("float" "integer" "key" "list" "rotation" "string" "vector"))
+             (x-keywords '("Cmd line" "Build fingerprint" "ABI" "Build type" "at " "native:" "kernel:" "Command line"))
+             (x-types '("utm=" "stm=" "tid="))
              (x-constants '("ACTIVE" "AGENT" "ALL_SIDES" "ATTACH_BACK"))
-             (x-events '("held by" "at_target" "attach"))
-             (x-functions '("llAbs" "llAcos" "llAddToLandBanList" "llAddToLandPassList"))
+             (x-events '("held by" "epoll" "held by thread" "waiting to lock" "locked"))
+             (x-functions '("getAndExecuteCommand" "waitForResponse" "pthread_mutex_lock"))
 
              ;; generate regex string for each category of keywords
              (x-keywords-regexp (regexp-opt x-keywords 'words))
@@ -43,7 +44,9 @@
 		  ;; reference http://ergoemacs.org/emacs/elisp_font_lock_mode.html
 		  ("\\[[^\]]+\\]" . font-lock-comment-face)
 		  ("^<[^>]+>" . font-lock-comment-face)
-		  ("=\\([0-9]+\\)" . (1 font-lock-keyword-face))
+		  ("<aka>\\([^<]+?\\)</aka>" . (1 font-lock-keyword-face))
+		  ("=\\([0-9]+\\)" . (1 font-lock-variable-name-face))
+		  ("^=[^$]+" . font-lock-variable-name-face)
           (,x-types-regexp . font-lock-type-face)
           (,x-constants-regexp . font-lock-constant-face)
           (,x-events-regexp . font-lock-builtin-face)
@@ -53,12 +56,22 @@
           ;; in general, put longer words first
           )))
 
+(defvar android-trace-mode-syntax-table nil "Syntax table for `android-trace-mode'.")
+
+(setq android-trace-mode-syntax-table
+      (let ( (synTable (make-syntax-table)))
+        ;; python style comment: “# …”
+        (modify-syntax-entry ?= "<" synTable)
+        (modify-syntax-entry ?\n ">" synTable)
+        synTable))
+
 ;;;###autoload
 (define-derived-mode android-trace-mode text-mode  "android trace mode"
   "Major mode for editing LSL (Linden Scripting Language)…"
 
   ;; code for syntax highlighting
-  (setq font-lock-defaults '((android-trace-font-lock-keywords))))
+  (setq font-lock-defaults '((android-trace-font-lock-keywords)))
+  (set-syntax-table android-trace-mode-syntax-table))
 
 
 (setq mode-line-format (delete (assoc 'which-func-mode
@@ -100,4 +113,12 @@
 
 (setq-default which-func-unknown "N/A")
 
+;; refs https://www.gnu.org/software/emacs/manual/html_node/elisp/Auto-Major-Mode.html
+;; filename startup with something
+(add-to-list 'auto-mode-alist '("/dumptrace_[\\.]*" . android-trace-mode))
+(add-to-list 'auto-mode-alist '("/anr_[\\.]*" . android-trace-mode))
+(add-to-list 'auto-mode-alist '("/bugreport_[\\.]*" . android-trace-mode))
+;; endup with .trace
+(add-to-list 'auto-mode-alist '("\\.trace\\'" . android-trace-mode))
+;;(add-to-list 'auto-mode-alist '("anr[[:ascii:]]*\\'" 'android-trace-mode))
 (provide 'setup-bugreport)
